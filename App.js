5fe43ea68c97b47d49ba67ddd5f6e17d9ec3f7c7
m.karimi <maedekarimi69110@gmail.com>
2023-05-29 14:47:17 +0330

    handle first project with react native

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [outputText, setOutputText] = useState("default text");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text style={{ fontSize: 50, color: "red" }}>mohammad brim kone</Text>
+      <Text style={{ fontSize: 50, color: "red" }}>{outputText}</Text>
+      <Button
+        title="change the text"
+        onPress={() => setOutputText("the text changed")}
+      />
     </View>
   );
 }
